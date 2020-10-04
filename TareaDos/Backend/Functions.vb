fb2e6eb4950957863886df43ec950287b7c9def8
Antonio Benavides <g.antoniobh@gmail.com>
2020-10-03 23:06:32 -0600

    add Functions

--- TareaDos/Backend/Functions.vb
+++ TareaDos/Backend/Functions.vb
@@ -1,3 +1,36 @@
-﻿Module Functions
+﻿MImports System.Drawing.Text
+Imports System.IO
+Module FUNCTIONS
+    Sub BOLD()
+
+    End Sub
+
+    Sub ITALIC()
+
+    End Sub
+
+    Sub UNDERLINE()
+
+    End Sub
+
+    Sub SHADOW()
+        NOTE.RICHTEXT_PAD.SelectionBackColor = Color.Yellow
+    End Sub
+
+
+    Sub ALIGN_LEFT()
+        NOTE.RICHTEXT_PAD.SelectionAlignment = HorizontalAlignment.Left
+    End Sub
+
+    Sub ALIGN_CENTER()
+        NOTE.RICHTEXT_PAD.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+
+
+    Sub ALIGN_RIGHT()
+        NOTE.RICHTEXT_PAD.SelectionAlignment = HorizontalAlignment.Right
+    End Sub
+
+
 
 End Module
